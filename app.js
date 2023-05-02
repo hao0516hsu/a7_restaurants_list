eba456f523197057857c7f4143cad3ce26c87907
hao0516hsu <chhsu0516@gmail.com>
2023-05-02 23:30:03 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 // 載入JSON檔改成載入種子資料
 const Restaurant = require('./models/restaurant')
-
+// 載入Body-parser
+const bodyParser = require('body-parser')
 // dotenv設定
 if (process.env.MONGODB_URI !== 'production') {
   require('dotenv').config()
@@ -32,6 +33,8 @@ app.set('view engine', 'handlebars')
 // 設定靜態檔案路徑
 app.use(express.static('public'))
 
+// bodyParser設定
+app.use(bodyParser.urlencoded({ extended: true }))
 // 設定首頁的路由
 app.get('/', (req, res) => {
   Restaurant.find()
@@ -40,25 +43,47 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 // 設定說明頁的路由
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant_id = req.params.restaurant_id
-  const restaurant = restaurantsList.results.find(restaurant => {
-    return restaurant.id.toString() === restaurant_id
-  })
-  res.render('show', { restaurant: restaurant })
-})
-// 設定查詢頁的路由
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  // 若空白搜尋，轉成首頁的路由
-  if (!keyword.trim()) {
-    return res.redirect("/")
-  }
+// app.get('/restaurants/:restaurant_id', (req, res) => {
+//   const restaurant_id = req.params.restaurant_id
+//   const restaurant = restaurantsList.results.find(restaurant => {
+//     return restaurant.id.toString() === restaurant_id
+//   })
+//   res.render('show', { restaurant: restaurant })
+// })
+// // 設定查詢頁的路由
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword
+//   // 若空白搜尋，轉成首頁的路由
+//   if (!keyword.trim()) {
+//     return res.redirect("/")
+//   }
   
-  const restaurants = restaurantsList.results.filter(restaurant => {
-    return restaurant.name.trim().toLowerCase().includes(keyword.trim().toLowerCase()) || restaurant.category.trim().toLowerCase().includes(keyword.trim().toLowerCase())
+//   const restaurants = restaurantsList.results.filter(restaurant => {
+//     return restaurant.name.trim().toLowerCase().includes(keyword.trim().toLowerCase()) || restaurant.category.trim().toLowerCase().includes(keyword.trim().toLowerCase())
+//   })
+//   res.render('index', { restaurants: restaurants, keyword: keyword })
+// })
+// 新增頁 (NOTE: 新增頁要在DETAIL之前)
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const restaurant = req.body
+  return Restaurant.create({
+    id: restaurant.restaurant_id,
+    name: restaurant.name,
+    name_en: restaurant.name_en,
+    category: restaurant.category,
+    image: restaurant.image,
+    location: restaurant.location,
+    phone: restaurant.phone,
+    google_map: restaurant.google_map,
+    rating: restaurant.rating,
+    description: restaurant.description
   })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 // 設定啟動伺服器相關
 app.listen(port, () => {
